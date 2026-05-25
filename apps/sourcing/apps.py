c83d92dccbb4254035459ad8781e9a59ad393cb6
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-05-25 14:13:00 +0500

    feat(sourcing): SourcingConfig app config (Module 6)

---
+++ apps/sourcing/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class SourcingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.sourcing'
+    label = 'sourcing'
+    verbose_name = 'Sourcing & Tendering'
