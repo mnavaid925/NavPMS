d2a6c1d66874e25502e1e61870d248b90e9ad6a8
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-06-04 23:13:56 +0500

    feat(grn): GoodsReceiptConfig app config

---
+++ apps/goods_receipt/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class GoodsReceiptConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.goods_receipt'
+    label = 'goods_receipt'
+    verbose_name = 'Goods Receipt & Inspection'
