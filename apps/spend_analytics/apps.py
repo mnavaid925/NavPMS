47fdf0b1cf24ca79a8dca0858bc845ada5af0704
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-06-06 15:44:45 +0500

    feat(spend_analytics): AppConfig (Module 15)

---
+++ apps/spend_analytics/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class SpendAnalyticsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.spend_analytics'
+    label = 'spend_analytics'
+    verbose_name = 'Spend Analytics & Reporting'
