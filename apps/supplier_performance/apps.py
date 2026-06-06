995a537a6e6ff22750959fc9d1515a7276606edd
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-06-06 17:10:07 +0500

    feat(supplier-performance): SupplierPerformanceConfig

---
+++ apps/supplier_performance/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class SupplierPerformanceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.supplier_performance'
+    label = 'supplier_performance'
+    verbose_name = 'Supplier Performance & Evaluation'
